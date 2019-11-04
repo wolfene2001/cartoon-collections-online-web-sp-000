8781cefc33b2cd40253a3ae1e7012debb28516f4
wolfene2001 <wolfene2001@yahoo.com>
2019-11-04 11:52:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(names)
     puts names
   end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+
 end
 
 def long_planeteer_calls# code an argument here
