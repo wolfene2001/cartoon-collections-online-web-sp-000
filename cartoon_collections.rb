09ef3f310794df7b012cea855d960a269ff6b500
wolfene2001 <wolfene2001@yahoo.com>
2019-11-04 14:39:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,11 @@ def roll_call_dwarves(names)
   end
 
 def summon_captain_planet(array)
-
+  words = []
+  array.length = 3
+  words.map! {|word|capitalize}
+  collect {w|w + "!"}
+  end
 end
 
 def long_planeteer_calls# code an argument here
