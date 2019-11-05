17bbc1d30021f819b0a7194c7988d703ac0c15a2
wolfene2001 <wolfene2001@yahoo.com>
2019-11-05 16:35:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,3 @@
-require "pry"
 def roll_call_dwarves(names)
   dwarves = ["Dopey", "Grumpy", "Bashful"]
   dwarves.each_with_index do |name, index|
@@ -12,16 +11,15 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-
   if array.select {|word|word.length > 4}.length > 0
-
   return true
 else
   return false
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  result = cheese_types.find {|cheese|array.include?(cheese)}
 end
